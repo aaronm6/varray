62e071fd72b47e79d917cf58c353c6f5fd48b123
Aaron M. <aaronm6@gmail.com>
2025-12-03 08:17:13 -0800

    Updated docstring

--- varray.py
+++ varray.py
@@ -1,21 +1,27 @@
 # BSD 3-Clause License; see https://github.com/aaronm6/varray/blob/main/LICENSE
 """
-varray (variable array): A light-weight array type with numpy ndarrays as the backend, 
-which supports 2d arrays with rows of variable length.
+varray (variable array): 
 
-Why, when awkward (awk) arrays exist?  awk arrays are efficient and versatile and should be
-used when one's use case aligns with their capabilities.  However, I found myself avoiding
-them in my own work for two reasons:
+A light-weight array type that supports numpy-like arrays in which the last dimension has
+variable length.
+
+Why reinvent the wheel, when awkward (awk) arrays exist?  awk arrays are efficient and versatile
+and are an excellent tool when one's needs align with the features they provide.  However, I 
+found myself avoiding them in my own work for two reasons:
+
     1: awk arrays are IMMUTABLE, and hence read-only.  This means they're great to read from,
-       but what if you actually want to use them to store data in a script?  For example, 
-       you want to fill rows in a loop?  This used to be possible in earlier versions of awk, 
-       but has since been removed.  This is my main motivation for creating varray: awk arrays' 
+       but tricky to use if you actually want to use them in a script or notebook.  For
+       example, a common usage of numpy arrays is to initialize an empty 2d array with 
+       np.empty(...) and then fill in the rows in a loop.  This is possible with numpy
+       arrays because they are mutable.  But one cannot do this with awk arrays.  This used 
+       to be possible in earlier versions of awk, but this important functionality has since 
+       been removed.  This is my main motivation for creating varray: awk arrays' 
        immutability makes them unusable for the vast majority of my own use cases.
     2: awk is a large package that involves c++ code and all of its plethora of functionality
        might not always be needed.  Hence the desire for a light-weight alternative that
        involves python code only.
 
-The focus here is on efficient and light-weight implementation of limited capabilities, 
+The focus here is on a simple and light-weight implementation of limited capabilities, 
 rather than a breadth of many capabilities.  For example, numpy arrays and ak arrays support 
 efficient slicing, where a slice of an array produces a new "view" to the underlying data, 
 but does not copy the data.  So too does varray work with array "views".  Data are only 
